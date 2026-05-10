e0f13d8a4088255c6fb49928d6ff90c7c55ea64c
alvaro <luisalvaro@outlook.com>
2026-05-10 00:16:43 -0500

    Implementación CI con GitHub Actions

--- obj/Debug/net8.0/ClinicaAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/ClinicaAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClinicaAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd490370a716fd9b58b3f57d6f5e4340fa41eed8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClinicaAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClinicaAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
