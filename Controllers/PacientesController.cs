cd490370a716fd9b58b3f57d6f5e4340fa41eed8
alvaro <luisalvaro@outlook.com>
2026-05-09 23:56:40 -0500

    Primer commit ClinicaAPI

---
+++ Controllers/PacientesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ClinicaAPI.Data;
+using ClinicaAPI.Models;
+
+namespace ClinicaAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PacientesController : ControllerBase
+    {
+        private readonly ClinicaContext _context;
+
+        public PacientesController(ClinicaContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_context.Pacientes.ToList());
+        }
+
+        [HttpPost]
+        public IActionResult Post(Paciente paciente)
+        {
+            _context.Pacientes.Add(paciente);
+            _context.SaveChanges();
+
+            return Ok(paciente);
+        }
+    }
+}
